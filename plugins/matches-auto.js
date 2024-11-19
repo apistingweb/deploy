0860990615aa5706673cf37e0ef6930f2421595f
apistingweb <143703659+apistingweb@users.noreply.github.com>
2024-11-19 15:28:49 +0200

    Create matches-auto.js

---
+++ plugins/matches-auto.js
@@ -0,0 +1,7 @@
+/*==================================
+~> Name Plugin       : Table Matches API
+~> Version:          : 2024 / 1.0 - STABLE
+~> Last Updated      : 15 - 11 - 2024   : ->  02:00 AM  +02 GMT
+~> Developer By      : STING WEB - Facebook Page   : ->  https://www.facebook.com/stingweb.eg
+~> Desgin URL        : https://www.sting-web.com
+=========== [ STING WEB ] ==========*/
